f89a5520813e2d1e7c1020354442fb82e622fc1c
Mufti Ramdhani <muftiramdhani@gmail.com>
2021-11-14 20:24:50 +0700

    finish migration

--- database/migrations/2021_11_14_115630_create_lists_table.php
+++ database/migrations/2021_11_14_115630_create_lists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateListsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('lists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('color_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->string('name');
-            $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
         });
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('lists');
     }
 }
---
+++ database/migrations/2021_11_14_115853_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('list_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name');
+            $table->dateTime('reminder_datetime');
+            $table->date('due_date');
+            $table->text('attachment_url');
+            $table->text('note');
+            $table->boolean('is_completed');
+            $table->boolean('is_my_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
